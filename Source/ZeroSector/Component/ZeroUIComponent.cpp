a6e18d111bde3389a0b727313b2d7beaeda32c34
kimkyungjae1112 <rudwo4861@naver.com>
2025-03-28 10:37:11 +0900

    #31 merge conflict solve

--- Source/ZeroSector/Component/ZeroUIComponent.cpp
+++ Source/ZeroSector/Component/ZeroUIComponent.cpp
@@ -18,7 +18,6 @@ void UZeroUIComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
 }
 
 void UZeroUIComponent::OperationNextButtonClick()
@@ -46,16 +45,29 @@ void UZeroUIComponent::FadeInAndOutDisplay()
 
 void UZeroUIComponent::ToggleNoteDisplay()
 {
+	IZeroUIComponentInterface* Interface = Cast<IZeroUIComponentInterface>(GetOwner());
+	if (Interface == nullptr) return;
+
 	if (bIsNoteToggle)
 	{
 		NoteWidgetPtr->RemoveFromParent();
 		bIsNoteToggle = false;
+		Interface->GetOwnerController()->InputModeGameOnly();
+		
 	}
 	else
 	{
 		NoteWidgetPtr = CreateWidget<UZeroNoteWidget>(GetWorld(), NoteWidgetClass);
 		NoteWidgetPtr->AddToViewport();
 		bIsNoteToggle = true;
+
+		// 게임 + UI 모드로 전환하되, 키보드 포커스를 Note 위젯에 주지 않음
+		FInputModeGameAndUI InputMode;
+		InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+		InputMode.SetHideCursorDuringCapture(false);
+		InputMode.SetWidgetToFocus(nullptr); // 또는 특정 포커스 위젯
+		Interface->GetOwnerController()->SetInputMode(InputMode);
+		Interface->GetOwnerController()->bShowMouseCursor = true;
 	}
 }
 
@@ -121,7 +133,7 @@ void UZeroUIComponent::ProvisoInteract()
 
 		if (NoteWidgetPtr)
 		{
-			NoteWidgetPtr->SetNoteInfo(ProvisoData.ProvisoName.ToString(), ProvisoData.Description);
+			NoteWidgetPtr->SetNoteInfo(ProvisoData);
 		}
 	}
 
