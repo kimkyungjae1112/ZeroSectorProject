09028208f728ff2f10a335cf78ac25792849e68b
kimkyungjae1112 <rudwo4861@naver.com>
2025-05-15 11:33:21 +0900

    #88 TakeDamage 에서 Niagara Activate

--- Source/ZeroSector/Character/Zombie/ZeroCharacterBaseZombie.cpp
+++ Source/ZeroSector/Character/Zombie/ZeroCharacterBaseZombie.cpp
@@ -5,6 +5,8 @@
 #include "Component/ZeroStatComponent.h"
 #include "Animation/ZeroAnimInstanceZombie.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "NiagaraComponent.h"
+#include "Components/AudioComponent.h"
 
 AZeroCharacterBaseZombie::AZeroCharacterBaseZombie()
 {
@@ -14,6 +16,15 @@ AZeroCharacterBaseZombie::AZeroCharacterBaseZombie()
     GetCharacterMovement()->MaxWalkSpeed = 0.f;
 
     StatComp = CreateDefaultSubobject<UZeroStatComponent>(TEXT("Stat Component"));
+
+    NiagaraComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Niagara Component"));
+    NiagaraComp->SetupAttachment(GetMesh());
+    NiagaraComp->SetRelativeLocation(FVector(0.f, 0.f, 100.f));
+    NiagaraComp->bAutoActivate = false;
+
+    AudioComp = CreateDefaultSubobject<UAudioComponent>(TEXT("Audio Component"));
+    AudioComp->SetupAttachment(GetMesh());
+    AudioComp->bAutoActivate = false;
 }
 
 void AZeroCharacterBaseZombie::BeginPlay()
@@ -29,6 +40,7 @@ void AZeroCharacterBaseZombie::BeginPlay()
         ZombieAnim->AnimPoseType = AnimPoseType;
         ZombieAnim->AnimIndex = AnimIndex;
     }
+
 }
 
 float AZeroCharacterBaseZombie::GetAIAttackRange()
@@ -72,6 +84,8 @@ void AZeroCharacterBaseZombie::AttackTwoByAI()
 float AZeroCharacterBaseZombie::TakeDamage(float Damage, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
 {
     float SuperResult = Super::TakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser);
+
+    NiagaraComp->Activate();
 
     return 0.0f;
 }
